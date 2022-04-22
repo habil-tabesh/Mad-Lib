48cce64e84e2ac3181fb564632807a20cdd6f773
Habil Tabesh <habil.tabesh134@gmail.com>
2022-04-22 02:33:14 -0400

    js added

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ function createParagraph() {
     var wordTwo = document.getElementById("text2").value;
     var wordThree = document.getElementById("text3").value;
     var wordFour = document.getElementById("text4").value;
-    var wordFive = document.getElementById("tex5").value;
+    var wordFive = document.getElementById("text5").value;
     var wordSix = document.getElementById("text6").value;
     var wordSeven = document.getElementById("text7").value;
     var wordEight = document.getElementById("text8").value;
@@ -17,11 +17,10 @@ function createParagraph() {
     var wordThirteen = document.getElementById("text13").value;
     
 
-    console.log(wordOne,wordTwo,wordThree,wordFour,wordFive,wordSix,wordSeven,wordEight,wordNine,wordTen,wordEleven,wordTwelve,wordThirteen)
 
-    var paragraph = "<p> Dear "+wordOne+", The first time I saw you my heart "+wordTwo+" with joy. We were in "+wordThree+" class and you raised you "+wordFour+" to ask a question. Your voice sounded like "+wordFive+". Then I noticed your "+wordSix+" "+wordSeven+". It is so "+wordEight+"! Overall, I "+wordNine+" you. I would tell you who I am but I'm too "+wordTen+". Just know that the "+wordEleven+" you reciece on Valentine's Day is from me. I hope you think it's "+wordTwelve+"      Love,        Your "+wordThirteen+" Admirer </p>"
+    var paragraph = " Dear "+ wordOne +", " + "<br>" +"The first time I saw you my heart " + wordTwo + " with joy. We were in " + wordThree + " class and you raised your " + wordFour + " to ask a question. Your voice sounded like " + wordFive+". Then I noticed your "+ wordSix + "  " + wordSeven+". It is so " + wordEight+"! Overall, I " + wordNine + " you. I would tell you who I am but I'm too "+ wordTen +". Just know that the "+ wordEleven +" you reciece on Valentine's Day is from me. I hope you think it's " + wordTwelve+  "<br>" +  "      Love,"  + "<br>" +      "Your " + wordThirteen + " Admirer "
 
-    console.log(paragraph);
+   
 
     document.getElementById("answer").innerHTML = paragraph;
 }
